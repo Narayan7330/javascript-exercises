3ce34d81a60552b4bae357a4da90dacde17b0766
Narayan7330 <narayanmali7330@gmail.com>
2025-03-30 17:08:12 +0000

    done with 4th ex

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr1,...arr2) {
+
+  return arr1.filter(function(item){
+   return !arr2.includes(item);
+  })
 };
 
 // Do not edit below this line

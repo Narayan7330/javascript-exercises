4c7e1126f4fcfbee78010229ce1d36e2ab2a6472
Narayan7330 <narayanmali7330@gmail.com>
2025-03-30 19:12:49 +0000

    done with 7th ex

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  const degreecel = (temp - 32)*5/9;
+  const finalTemp = Math.round(degreecel * 10) / 10
+  return finalTemp;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  const degreefahrenite = (temp*9/5+32);
+  const finalTemp = Math.round(degreefahrenite * 10) / 10
+  return finalTemp;
 };
 
 // Do not edit below this line

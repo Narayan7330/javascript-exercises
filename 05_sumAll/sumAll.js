07760b610b9e6d4eab0b11b40bd8d7a75631f914
Narayan7330 <narayanmali7330@gmail.com>
2025-03-30 17:55:06 +0000

    done with 5th ex

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(n1,n2) {
+
+if(n1<0||n2<0||!Number.isInteger(n1) || !Number.isInteger(n2)||isNaN(n1)
+||isNaN(n2) )
+{
+  return "ERROR";
+}
+else if(n1<n2)
+{let sum = 0;
+for(let i=n1;i<n2+1;i++){
+  sum = sum + i;
+}
+return sum;} 
+else if(n2<n1){
+  let sum = 0;
+  for(let i=n2;i<n1+1;i++){
+    sum = sum + i;
+  }
+  return sum;
+
+}
 
 };
 

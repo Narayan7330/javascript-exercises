ccf03c59a4db5e62d6014b1d154d5fe686694292
Narayan7330 <narayanmali7330@gmail.com>
2025-06-08 13:21:01 +0000

    dont with palindrome exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+  let originalString = string;
+  let withoutPuncutuaionOriginalString = originalString.replace(/[^a-z0-9]/gi, "")
+  const reversedString = withoutPuncutuaionOriginalString.split('').reverse().join('');
+
+if(withoutPuncutuaionOriginalString.toUpperCase() === reversedString.toUpperCase()){
+  return true;
+} 
+else{
+return false;
+}
 };
 
 // Do not edit below this line

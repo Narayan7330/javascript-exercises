420a701509499e890c296835049a626bb4890cbf
Narayan7330 <narayanmali7330@gmail.com>
2025-06-08 12:09:06 +0000

    done with calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add = function(a,b) {
+  
+  return a+b;
+  
 
 };
 
-const power = function() {
+const subtract = function(a,b) {
 	
+  return a-b;
 };
 
-const factorial = function() {
+const sum = function(array) {
 	
+return array.reduce((prev,next) => prev + next , 0);
+
+
+};
+
+const multiply = function(array) {
+
+  return array.reduce((prev,next) => prev * next,1);
+
+};
+
+const power = function(a,b) {
+  return a ** b;
+};
+
+const factorial = function(n) {
+  if (n>=1){
+let result = 1;
+  for (let i=1;i<=n;i++){
+    result *= i;
+  }
+  return result;
+  }
+  else{
+    return 1;
+  }
+
 };
 
 // Do not edit below this line

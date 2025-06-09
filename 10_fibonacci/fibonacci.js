cc89bfd29e8e1ac79ed644d1a6294d6e6362a26c
Narayan7330 <narayanmali7330@gmail.com>
2025-06-09 08:35:46 +0000

    done with exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+if(n < 0) return "OOPS";
+if(n==0) return 0;
+if(n ==1 || n == 2) return 1;
 
+
+let previousNumberOne = 1;
+let previousNumberTwo = 1;
+for(let i = 3;i<= n;i++){
+
+  let currentNumber = previousNumberOne + previousNumberTwo ;
+
+  previousNumberOne = previousNumberTwo;
+  previousNumberTwo = currentNumber;
+  
+}
+
+return previousNumberTwo;
 };
 
 // Do not edit below this line
